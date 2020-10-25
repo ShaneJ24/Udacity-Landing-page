acffa5b95238e13019aa1a19e00f7f4e8a56b413
ShaneJ24 <70983328+ShaneJ24@users.noreply.github.com>
2020-10-25 19:28:50 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,39 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-/**
- * Define Global Variables
- *
- */
 const navbarMenu = document.getElementById("navbar__list");
 //select sections
 const sections = document.querySelectorAll("section");
+
 //build a nav
 function createNav() {
-  for (let item of sections) {
-    let section = document.createElement("li");
-    section.className = "menu__link";
-    section.innerText = item.dataset.nav;
-    section.addEventListener('click', function () {
-            item.scrollIntoView({behavior: "smooth"})
-        })
-    navbarMenu.appendChild(section);
-  }
+    for (let item of sections) {
+        let section = document.createElement("li");
+        section.className = "menu__link";
+        section.innerText = item.dataset.nav;
+        section.setAttribute('data-id', item.id);
+        /*navigation scroll*/
+        section.addEventListener("click", function () {
+            item.scrollIntoView({behavior: "smooth"});
+        });
+        navbarMenu.appendChild(section);
+    }
 }
+/* Scrolls feature and highlight each section being viewed*/
 createNav();
-const section1 = document.getElementsByClassName("firstSection");
-const section2 = document.getElementsByClassName("secondSection");
-const section3 = document.getElementsByClassName("thirdSection");
-/*Navigation scroll*/
-function myfunction() {
-var element = document.getElementById("menu__link");
-element.scrollIntoView("landing__container");
-element.scrollIntoView({behavior: "smooth"});
-document.addEventListener('click', section1)
-}
+
+jQuery(window).scroll(function () {
+    var position = jQuery(this).scrollTop();
+
+    jQuery('section').each(function () {
+        var target = jQuery(this).offset().top;
+
+        var id = jQuery(this).attr('id');
+        jQuery('#navbar__list li[data-id=' + id + ']').removeClass('active');
+        if (position >= target - 120) {
+            jQuery('#navbar__list li').removeClass('active');
+            jQuery('#navbar__list li[data-id=' + id + ']').addClass('active');
+
+            jQuery('section').removeClass('active-section');
+            jQuery('#'+id).addClass('active-section');
+        }
+    });
+
+});
