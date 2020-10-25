d8278339abf085be2128e0d182b2a22c520ea8f5
ShaneJ24 <70983328+ShaneJ24@users.noreply.github.com>
2020-10-25 18:51:06 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,44 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
-
+ *
+ */
 /**
  * Define Global Variables
- * 
-*/
-const section1 = document.getElementById("sectionOne");
-const section2 = document.getElementById("sectionTwo");
-const section3 = document.getElementById("sectionThree");
-
-/**
- * End Global Variables
-
- * Begin Events
- * 
-*/
-/* Created an event so when the section in the Nav is selected it listens for the click
-   and then scrolls to that section*/
-section1.addEventListener("click", function(){
-    window.location = "index.html#section1"
-})
-
-section2.addEventListener("click", function(){
-    window.location = "index.html#section2"
-})
-
-section3.addEventListener("click", function(){
-    window.location = "index.html#section3"
-})
+ *
+ */
+const navbarMenu = document.getElementById("navbar__list");
+//select sections
+const sections = document.querySelectorAll("section");
+//build a nav
+function createNav() {
+  for (let item of sections) {
+    let section = document.createElement("li");
+    section.className = "menu__link";
+    section.innerText = item.dataset.nav;
+    section.addEventListener('click', function () {
+            item.scrollIntoView({behavior: "smooth"})
+        })
+    navbarMenu.appendChild(section);
+  }
+}
+createNav();
+const section1 = document.getElementsByClassName("firstSection");
+const section2 = document.getElementsByClassName("secondSection");
+const section3 = document.getElementsByClassName("thirdSection");
+/*Navigation scroll*/
+function myfunction() {
+var element = document.getElementById("menu__link");
+element.scrollIntoView("landing__container");
+element.scrollIntoView({behavior: "smooth"});
+document.addEventListener('click', section1)
+}
