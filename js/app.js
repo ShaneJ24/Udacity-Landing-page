39c2c568f523aa6cbe52b7be2b53c8a3dd766da6
ShaneJ24 <70983328+ShaneJ24@users.noreply.github.com>
2020-10-26 17:05:03 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,36 +4,40 @@ const sections = document.querySelectorAll("section");
 
 //build a nav
 function createNav() {
-    for (let item of sections) {
-        let section = document.createElement("li");
-        section.className = "menu__link";
-        section.innerText = item.dataset.nav;
-        section.setAttribute('data-id', item.id);
-        /*navigation scroll*/
-        section.addEventListener("click", function () {
-            item.scrollIntoView({behavior: "smooth"});
-        });
-        navbarMenu.appendChild(section);
-    }
+  for (let item of sections) {
+    let section = document.createElement("li");
+    section.className = "menu__link";
+    section.innerText = item.dataset.nav;
+    section.setAttribute("data-id", item.id);
+    /*navigation scroll*/
+    section.addEventListener("click", function () {
+      item.scrollIntoView({ behavior: "smooth" });
+    });
+    navbarMenu.appendChild(section);
+  }
 }
-/* Scrolls feature and highlight each section being viewed*/
+
 createNav();
 
-jQuery(window).scroll(function () {
-    var position = jQuery(this).scrollTop();
-
-    jQuery('section').each(function () {
-        var target = jQuery(this).offset().top;
-
-        var id = jQuery(this).attr('id');
-        jQuery('#navbar__list li[data-id=' + id + ']').removeClass('active');
-        if (position >= target - 120) {
-            jQuery('#navbar__list li').removeClass('active');
-            jQuery('#navbar__list li[data-id=' + id + ']').addClass('active');
-
-            jQuery('section').removeClass('active-section');
-            jQuery('#'+id).addClass('active-section');
-        }
-    });
-
+window.addEventListener("scroll", function (e) {
+  var position = window.scrollY;
+  document.querySelectorAll("section").forEach((item) => {
+    var target = item.offsetTop;
+    var id = item.id;
+    document
+      .querySelector("#navbar__list li[data-id=" + id + "]")
+      .classList.remove("active");
+    if (position >= target - 120) {
+      document.querySelectorAll("#navbar__list li").forEach((item) => {
+        item.classList.remove("active");
+      });
+      document.querySelectorAll("section").forEach((item) => {
+        item.classList.remove("active-section");
+      });
+      document
+        .querySelector("#navbar__list li[data-id=" + id + "]")
+        .classList.add("active");
+      document.querySelector("#" + id).classList.add("active-section");
+    }
+  });
 });
